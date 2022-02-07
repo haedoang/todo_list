4d0bcd510e8b136ec8b871e05588d0e0fe33254a
haedoang <haedoang@gmail.com>
2022-02-07 16:11:06 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+            update version.!!
         </a>
       </header>
     </div>
